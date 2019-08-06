4305b8a6decaf35ed262590fd5563dc0aed23505
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-08-06 18:33:40 +0300

    Program file created. Added IsValidJsonNumber and added null check to return invalid.

---
+++ JsonNumberValidator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JsonNumberValidator
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine(Convert.ToInt32("012"));
+        }
+
+        public static string IsValidJsonNumber(string introducedNumber)
+        {
+            if (introducedNumber == null)
+            {
+                return "Invalid";
+            }
+
+            return "Invalid";
+        }
+    }
+}
