475ef0bc40f6f211a9e83fa4adbd7a00d66d19f7
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-08-06 18:37:42 +0300

    Added IsValidJsonNumberWhen0ShouldReturnValid

--- JsonNumberValidator.Tests/ProgramTests.cs
+++ JsonNumberValidator.Tests/ProgramTests.cs
@@ -10,5 +10,11 @@ namespace JsonNumberValidator.Tests
         {
             Assert.Equal("Invalid", Program.IsValidJsonNumber(null));
         }
+
+        [Fact]
+        public void IsValidJsonNumberWhen0ShouldReturnValid()
+        {
+            Assert.Equal("Valid", Program.IsValidJsonNumber("0"));
+        }
     }
 }
